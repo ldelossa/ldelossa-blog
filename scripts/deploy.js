6173f7472734bd8d587c4094d3f08f65398e560d
ldelossa <louis.delos@gmail.com>
2020-06-17 01:08:08 -0400

    gh-pages publish

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+const ghpages = require('gh-pages');
+
+ghpages.publish(
+    '__sapper__/export',
+    {
+        branch: 'master',
+        repo: 'ldelossa-blog.git',
+        user: {
+            name: 'ldelossa',
+            email: 'louis.delos@gmail.com'
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+)
