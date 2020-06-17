86b995a66cc6249d908dc9373569768e26653afd
ldelossa <louis.delos@gmail.com>
2020-06-17 10:30:22 -0400

    try blog entries at root

--- src/routes/[slug].svelte
+++ src/routes/[slug].svelte
@@ -1,7 +1,8 @@
 <script context="module">
     export async function preload(page, session) {
         const { slug } = page.params;
-        const res = await this.fetch(`blog/${slug}.json`)
+        console.log(slug)
+        const res = await this.fetch(`${slug}.json`)
         const article = await res.json();
         return {article}
     }
