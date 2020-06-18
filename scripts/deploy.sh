5dbf9eacc17e96c5251798d826465be90b21db7a
ldelossa <louis.delos@gmail.com>
2020-06-18 14:07:08 -0400

    remove basename

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-BASE_NAME="ldelossa-blog"
-
 ROOT="$(git rev-parse --show-toplevel)"
 cd "$ROOT"
 HEAD="$(git symbolic-ref HEAD)"
 git symbolic-ref HEAD refs/heads/gh-pages
 git reset "$HEAD"
 git rm --cached -rf .
-BASE_NAME="$BASE_NAME" npx sapper export --basepath "$BASE_NAME" || exit 1
-cd __sapper__/export/"$BASE_NAME"
+npx sapper export || exit 1
+cd __sapper__/export/
 echo "www.ldelossa.is" > CNAME
 files=($(find -type f | cut -c 3-))
 for file in ${files[@]}; do hash=$(git hash-object -w $file); git update-index --add --cacheinfo 100644,$hash,$file; done
