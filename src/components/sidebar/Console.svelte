32e3400f1bd54b4313ec654de3585c520116bda3
ldelossa <louis.delos@gmail.com>
2020-07-03 22:24:44 -0400

    add cursor

--- src/components/sidebar/Console.svelte
+++ src/components/sidebar/Console.svelte
@@ -1,4 +1,19 @@
 <script>
+    import { onMount } from 'svelte';
+    onMount(() => {
+        var cursor = true;
+        var speed = 300;
+
+        setInterval(() => {
+           if(cursor) {
+             document.getElementById('cursor').style.opacity = 0;
+             cursor = false;
+           }else {
+             document.getElementById('cursor').style.opacity = 1;
+             cursor = true;
+           }
+        }, speed);
+    });
 </script>
 
 
@@ -10,6 +25,7 @@
         min-width: 230px;
         max-width: 275px;
         background: #333;
+        color: #737373;
     }
 </style>
 
@@ -20,5 +36,6 @@
 ├── linux
 ├── music
 └── software
+❯ <span id="cursor">▊</span>
     </code>
 </pre>
