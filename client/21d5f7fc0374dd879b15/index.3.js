83f44f88a002ea866667c8d8e56301bfc3bd6346
ldelossa <louis.delos@gmail.com>
2020-10-20 20:37:51 -0400

    Build

---
+++ client/21d5f7fc0374dd879b15/index.3.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{3:function(n,t,e){"use strict";e.r(t),e.d(t,"preload",(function(){return s}));var r=e(0);async function s(n,t){return this.redirect(302,"about")}class u extends r.a{constructor(n){super(),Object(r.D)(this,n,null,null,r.N,{})}}t.default=u}}]);
